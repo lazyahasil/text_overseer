40251dae6e09efeee8820d16fdf3b785b18c407b
Ahasil <lazyahasil+dev@gmail.com>
2017-03-07 18:20:01 +0900

    초기 커밋.

---
+++ nana-practice/MainWindow.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class MainWindow
+{
+public:
+	MainWindow();
+
+private:
+
+};
---
+++ nana-practice/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <array>
+#include <iostream>
+#include <memory>
+
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/button.hpp>
+#include <nana/gui/msgbox.hpp>
+
+
+
+int main()
+{
+	nana::form fm;
+
+
+
+	nana::exec();
+
+	return 0;
+}
